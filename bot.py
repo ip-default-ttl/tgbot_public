6f74b410a205e3f10791b0580bc7c5713ff53c05
Kirill <45688082+ip-default-ttl@users.noreply.github.com>
2019-11-13 17:03:16 +0300

    Добавлен бан и предупреждение за войсы

--- bot.py
+++ bot.py
@@ -22,8 +22,31 @@ def get_updates_json(request, offset_param):
     response = requests.get(request + 'getUpdates', data=params)
     return response.json()
 
+def Ban_User(chat, user):
+    now = datetime.datetime.now()
+    if int(now.month) == 11:
+        until = now.replace (month = 1)
+        until = now.replace (year = int(now.year+1))
+    else:
+        until = now.replace(month = int(now.month+1))
+    params = {
+    'chat_id' : chat,
+    'user_id' : user,
+    'until_date' : until,
+    'can_send_messages' : False,
+    'can_send_media_messages': False,
+    'can_send_polls': False,
+    'can_send_other_messages': False,
+    'can_add_web_page_previews': False,
+    'can_change_info': False,
+    'can_invite_users': False,
+    'can_pin_messages': False
+    }
+    response = requests.post (url + 'restrictChatMember', data = params )
+
 def main():
     offst=-1
+    warnlist = []
     while True:
         our_json=get_updates_json(url,offst)
         while ('result' not in our_json):
@@ -55,17 +78,34 @@ def main():
                         elif flag1:
                             send_message(get_chat_id(our_json['result'][i]), "[ + ] "+our_json['result'][i]['message']['from']['first_name']+" записался(-ась) на ОФП")
                         flag1 = True
-                elif ('voice' in buf):
+                elif ('voice' in buf) and ( (buf['chat']['type']=='group') or (buf['chat']['type']=='supergroup')):
+                    if (buf['from']['id'] in warnlist):
+                        Ban_User(buf['chat']['id'], buf['from']['id'])
+                        warnlist.remove(buf['from']['id'])
+                        flag3= True
+                        if ('username' in our_json['result'][i]['message']['from']):
+                            send_message(get_chat_id(our_json['result'][i]), "[ + ] "+"@"+our_json['result'][i]['message']['from']['username']+" Забанен(-a) за голосовые сообщения")
+                            flag3 = False
+                        elif ('last_name' in our_json['result'][i]['message']['from']) and flag3:
+                            send_message(get_chat_id(our_json['result'][i]), our_json['result'][i]['message']['from']['first_name']+" "+our_json['result'][i]['message']['from']['last_name']+" Забанен(-a) за голосовые сообщения")
+                            flag3 = False
+                        elif flag3:
+                            send_message(get_chat_id(our_json['result'][i]), our_json['result'][i]['message']['from']['first_name']+" Забанен(-a) за голосовые сообщения")
+                    else:
+                        flag2 = True
+                        if ('username' in our_json['result'][i]['message']['from']):
+                            send_message(get_chat_id(our_json['result'][i]), "@"+our_json['result'][i]['message']['from']['username']+", голосовые сообщения не приветствуются в групповом чате. Пожалуйста продублируйте то же самое, но текстом. Спасибо за понимание! (Вам выдано предупреждение!)")
+                            flag2 = False
+                            warnlist.append(buf['from']['id'])
+                        elif ('last_name' in our_json['result'][i]['message']['from']) and flag2:
+                            send_message(get_chat_id(our_json['result'][i]), our_json['result'][i]['message']['from']['first_name']+" "+our_json['result'][i]['message']['from']['last_name']+", голосовые сообщения не приветствуются в групповом чате. Пожалуйста продублируйте то же самое, но текстом. Спасибо за понимание! (Вам выдано предупреждение!)")
+                            flag2 = False
+                            warnlist.append(buf['from']['id'])
+                        elif flag2:
+                            send_message(get_chat_id(our_json['result'][i]), our_json['result'][i]['message']['from']['first_name']+", голосовые сообщения не приветствуются в групповом чате. Пожалуйста продублируйте то же самое, но текстом. Спасибо за понимание! (Вам выдано предупреждение!)")
+                            warnlist.append(buf['from']['id'])
                     flag2 = True
-                    if ('username' in our_json['result'][i]['message']['from']):
-                        send_message(get_chat_id(our_json['result'][i]), "@"+our_json['result'][i]['message']['from']['username']+", голосовые сообщения не приветствуются в групповом чате. Пожалуйста продублируйте то же самое, но текстом. Спасибо за понимание!")
-                        flag2 = False
-                    elif ('last_name' in our_json['result'][i]['message']['from']) and flag2:
-                        send_message(get_chat_id(our_json['result'][i]), our_json['result'][i]['message']['from']['first_name']+" "+our_json['result'][i]['message']['from']['last_name']+", голосовые сообщения не приветствуются в групповом чате. Пожалуйста продублируйте то же самое, но текстом. Спасибо за понимание!")
-                        flag2 = False
-                    elif flag2:
-                        send_message(get_chat_id(our_json['result'][i]), our_json['result'][i]['message']['from']['first_name']+", голосовые сообщения не приветствуются в групповом чате. Пожалуйста продублируйте то же самое, но текстом. Спасибо за понимание!")
-                    flag2 = True
+                    flag3=True
             offst = our_json['result'][msg_last_num]['update_id']
             offst += 1
             sleep (1)
